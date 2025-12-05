5cec7596c5115316a6b8cddf7aef04d6356a2070
Donemmynaija <adeyemie90@gmail.com>
2025-12-05 13:06:31 +0100

    Starting Real Estate Project

--- donemmynaijaapi/models.py
+++ donemmynaijaapi/models.py
@@ -1,57 +1,20 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# Create your models here:
-
-# ( dis is to mix django default user model with custom profile model)
-# class User(models.Model):
-#    pass # Placeholder for user-related fields
-
-# class Profile(models.Model):
-
-#    def __str__(self):
-#        pass # Placeholder for string representation
-
-# Django relationship with User model
-
-'''
-1-to-1 relationship: One user has one profile
-   (a student can have only one ID card)
-   models.OneToOneField(User, on_delete=models.CASCADE)
-
-One-to-many relationship: One user can have multiple posts
-    (a blogger can write multiple blog posts)
-    models.ForeignKey(User, on_delete=models.CASCADE)
-
-Many-to-many relationship: Users can follow multiple users and be followed by multiple users
-    (social media followers)
-    models.ManyToManyField(User, related_name='followers', blank=True)
-'''
-
-# SHIRT_SIZE = (
-#    ('S', 'Small'),
-#    ('M', 'Medium'),
-#    ('L', 'Large'),
-#    ('XL', 'Extra Large'),
-#)
+# Create your models here.
 
 GENDER = (
-    ('male', 'Male'),
-    ('female', 'Female'),
-    ('other', 'Other'),
+    ('Male','Male'),
+    ('Female','Female'),
 )
-
-
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    fullname = models.CharField(max_length=100)
-    username = models.CharField(max_length=100, unique=True)
+    fullname = models.CharField(max_length=50)
+    username = models.CharField(max_length=50, unique=True)
     email = models.EmailField()
-# gender = models.CharField(max_length=10, choices=[('male', 'Male'), ('female', 'Female'), ('other', 'Other')])
-    gender = models.CharField(max_length=10, choices=GENDER)
-    phone = models.CharField(max_length=15)
-    photo = models.ImageField(upload_to="profile_pictures/", default="https://randomuser.me/api/portraits/men/83.jpg", null=True, blank=True)
-#    profile_picture = models.ImageField(upload_to='profile_pictures/', null=True, blank=True)
-
+    gender = models.CharField(max_length=6, choices=GENDER)
+    phone = models.CharField(max_length=12)
+    photo = models.ImageField( upload_to="profile",default="https://randomuser.me/api/portraits/men/83.jpg" , null=True, blank=True)
+    
     def __str__(self):
         return self.fullname
--- donemmynaijaapi/serializers.py
+++ donemmynaijaapi/serializers.py
@@ -1,61 +1,46 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
-from .models import Profile
-from .utils import send_email
+from . models import Profile
+from . utils import send_email
 
-class UserSerializer(serializers.Serializer):
+
+class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['username', 'email']
+        fields = ["username","email"]
 
-class ProfileSerializer(serializers.Serializer):
-        class Meta:
-            model = UserSerializer()
-            fields = ['fullname', 'username', 'email', 'gender', 'phone', 'photo']
+class ProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserSerializer()
+        fields = ["fullname","username","email","gender","phone","photo"]
 
-class RegistrationSerializer(serializers.Serializer):
+class RegistrationSerializer(serializers.ModelSerializer):
     username = serializers.CharField(write_only=True)
     password1 = serializers.CharField(write_only=True)
     password2 = serializers.CharField(write_only=True)
     email = serializers.EmailField(write_only=True)
     class Meta:
-        model = Profile()
-        fields = ['user', 'fullname', 'username', 'email', 'gender', 'phone', 'photo']
+        model = Profile
+        fields=  ["fullname","username","password1","password2","email","gender","phone","photo"]
     
-    def validate(self, data):
-         if data['password1'] != data['password2']:
-            raise serializers.ValidationError("Passwords do not match.")
-         
-         return data
-         
-        #  if data['email'].exists():
-        #    return serializers.ValidationError("Email already in use.")
-
-        # if data['username'].exists():
-        #    return serializers.ValidationError("Username already in use.")
-
-        # if data['username'] in User.objects.all():
-        #    return serializers.ValidationError("Username already in use.")
+    def validate(self,data):
+        if data['password1'] != data['password2']:
+            raise serializers.ValidationError("Password does not match")
+        return data
     
-
     def create(self, validated_data):
-            username=validated_data.pop('username'),
-            email=validated_data.pop('email'),
-            password=validated_data.pop('password1'),
+        username= validated_data.pop('username')
+        email = validated_data.pop('email')
+        password = validated_data.pop('password1')
 
-            # pop is to remove and add to variables
-
-            user = User.objects.create_user(username=username, email=email, password=password)
-            profile = Profile.objects.create(
-               user=user,
-               fullname=validated_data['fullname'],
-               # validated is to get data from the serializer
-               gender=validated_data['gender'],
-               phone=validated_data['phone'],
-               photo=validated_data['photo']                                
-                                             
-          )
-            
-            # send email to the registered email
-            send_email(username, email)
-            return profile
+        user = User.objects.create_user(username=username,password=password, email=email)
+        profile = Profile.objects.create(
+            user = user,
+            fullname = validated_data['fullname'],
+            gender = validated_data['gender'],
+            phone = validated_data['phone'],
+            photo = validated_data['photo'],
+        )
+        # send email to the registered email
+        send_email(username,email)
+        return profile
